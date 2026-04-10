377aadb551612a73724300d66c593487cd0fd859
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2026-04-10 17:25:23 -0300

    Refactor: Melhoria na escrita do código do header
    - Componentizo os links do site para facilitar a implementação.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,8 @@
 import Link from "next/link";
-import { Button } from "../ui/button";
-import { useRouter } from "next/router";
-import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";;
+import { ActiveLink } from "../active-link";
 
 export function Header() {
-
-    const router = useRouter();
-    const isHomePage = router.pathname === '/';
-    const isBlogPage = router.pathname.startsWith('/blog');
 
   return (
     <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-backdrop-filters:bg-background/60">
@@ -15,18 +10,18 @@ export function Header() {
         <div className="flex h-16 items-center justify-between">
           <Link href="/">Logo</Link>
 
-          <nav className="">
-            <ul className="flex items-center gap-6">
-              <li>
-                <Link className={cn('text-sm font-medium transition-colors hover:text-blue-500', isHomePage? 'text-blue-500' : 'text-muted')} href="/">Início</Link>
-              </li>
-              <li>
-                <Link className={cn('text-sm font-medium transition-colors hover:text-blue-500', isBlogPage? 'text-blue-500' : 'text-muted')} href="/blog">Blog</Link>
-              </li>
-              <li>
-                <Button variant="secondary">Começar</Button>
-              </li>
-            </ul>
+          <nav className="flex items-center gap-6">
+            <ActiveLink href="/">
+              Início
+            </ActiveLink>
+            <ActiveLink href="/blog">
+              Blog
+            </ActiveLink>
+            <Button variant="secondary" asChild>
+              <Link href="/comecar">
+                Começar
+              </Link>
+            </Button>
           </nav>
         </div>
       </div>
