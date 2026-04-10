74b5665e38b57784118607f1359a6c94a34b92f2
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2026-04-10 14:07:01 -0300

    Componentes Link e Image
    - Aprendizado do componente Link para roteamento do projeto.
    - Aprendizado do componente Image para melhores importações e otimizações de imagem.

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Blog() {
     return (
         <div>
-            <h1>
-                Blog
-            </h1>
+            <div>
+                <h1>
+                    Blog
+                </h1>
+                <Image
+                    src="/assets/primeiro-post.png"
+                    width={400}
+                    height={400}
+                    alt="Imagem"
+                />
+            </div>
+            <Link href="/">Home</Link>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="w-full h-screen text-center font-bold text-3xl p-4">
       <h1 className="">
         Seja bem vindo!
       </h1>
+      <Link href="/blog">Ir para o Blog</Link>
     </div>
   );
 }
