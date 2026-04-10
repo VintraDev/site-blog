b73eba350daff08122b6f89b39b2dd586c9f6ffe
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2026-04-10 14:24:27 -0300

    feat: initial commit

--- src/pages/about/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function About() {
-  return (
-    <div className="w-full h-screen text-center font-bold text-3xl p-4">
-      <h1 className="">
-        About
-      </h1>
-    </div>
-  );
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div className="w-full h-screen text-center font-bold text-3xl p-4">
       <h1 className="">
         Seja bem vindo!
       </h1>
-      <Link href="/blog">Ir para o Blog</Link>
     </div>
   );
 }
