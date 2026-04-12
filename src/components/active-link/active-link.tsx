ebbf5fe195a848bd8787bd567689f7e0f47a135f
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2026-04-11 21:58:25 -0300

    Feat - Hero Section implementation
    - Implementa a hero section do site.

--- src/components/active-link/active-link.tsx
+++ src/components/active-link/active-link.tsx
@@ -9,9 +9,9 @@ type ActiveLinkProps = {
 export function ActiveLink({ children, href, ...rest }: ActiveLinkProps) {
 
     const router = useRouter();
-    const isCurrentPath = router.asPath === href || router.asPath === rest.as || router.asPath.startsWith(String(rest.as));
+    const isCurrentPath = router.asPath === href || router.asPath === rest.as;
 
     return (
-        <Link className={cn('text-sm font-medium transition-colors hover:text-blue-500', isCurrentPath ? 'text-blue-500' : 'text-muted')} href={href}>{children}</Link>
+        <Link className={cn('text-action-sm font-medium transition-colors hover:text-blue-200', isCurrentPath ? 'text-blue-200' : 'text-gray-100')} href={href}>{children}</Link>
     )
 }
