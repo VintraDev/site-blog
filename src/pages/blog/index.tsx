34453daa6e5ad12e3d5d02dbfe6d77396fac1815
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2026-04-19 21:17:00 -0300

    Feat - Search component implementation.

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,5 @@
+import { Search } from "@/components/ui/search";
+
 export default function BlogPage() {
     return (
         <div className="flex flex-col py-24 grow h-full">
@@ -13,11 +15,10 @@ export default function BlogPage() {
                             Dicas e estratégias para impulsionar seu negócio
                         </h1>
                     </div>
+                    {/* Search */}
+                    <Search />
                 </div>
             </header>
-
-
-            {/* Search */}
 
             {/* Listagem de posts */}
         </div>
