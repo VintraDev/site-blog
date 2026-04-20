87f3ece6efb46d2bbdfa55bc73de9468e39cad95
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2026-04-20 15:10:02 -0300

    Feat - Search input inprovment
    - Refactor no componente de pesquisa
    - Melhorias no componente search.

--- src/components/ui/search/search.tsx
+++ src/components/ui/search/search.tsx
@@ -1,13 +1,12 @@
 import { cn } from "@/lib/utils";
-import { SearchIcon } from "lucide-react";
+import { CircleX, SearchIcon } from "lucide-react";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 
 export function Search() {
 
     const router = useRouter();
-    const q = router.query.q as string;
-    const query = router.query.q as string;
+    const query = (router.query.q as string) ?? '';
 
     const handleSearch = useCallback((event: React.FormEvent) => {
         event.preventDefault();
@@ -27,15 +26,30 @@ export function Search() {
         });
     }
 
+    function resetSearch() {
+        router.push('/blog', undefined, {
+            shallow: true,
+            scroll: false,
+        })
+    }
+
     return (
-        <form className="relative group" onSubmit={handleSearch}>
+        <form className="relative group w-full md:w-60" onSubmit={handleSearch}>
             <SearchIcon className={cn(`text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300`, query? 'text-blue-300': '' )} />
             <input
                 type="text"
                 name=""
+                value={query}
                 onChange={hancleQueryChange}
-                className="h-10 w-72 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"
+                className="h-10 w-full md:w-60 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"
                 placeholder="Buscar" />
+
+            {query && (
+                <CircleX 
+                className={`text-gray-300 w-4 h-4 top-1/2 -translate-y-1/2 absolute right-3`}
+                onClick={resetSearch}
+                />
+            )}
         </form>
     )
 }
